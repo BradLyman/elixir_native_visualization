95cc58be28cdc63573f400ea69e3b21154c1e458
Bradley Lyman <lyman.brad3211@gmail.com>
2017-09-25 23:52:02 -0700

    Create Context Class
    The idea is to let most "global context" be managed by the context
    class. This class can be moved out of the file if it gets big, but it
    will keep all of the global setup/teardown in one place.

--- c_src/first_nif.cpp
+++ c_src/first_nif.cpp
@@ -1,31 +1,103 @@
 #include <erl_nif.h>
 #include <SDL.h>
 
+#include <iostream>
+
+class Context
+{
+private:
+    Context(ErlNifEnv* env)
+        : ok{enif_make_atom(env, "ok")}
+    {
+        auto resultCode = SDL_Init(SDL_INIT_EVERYTHING);
+        if (resultCode < 0)
+        {
+            throw std::runtime_error{SDL_GetError()};
+        }
+        std::cout << "SDL_Init success!" << std::endl;
+    }
+
+    ~Context()
+    {
+        SDL_Quit();
+    };
+
+    Context(const Context&) = delete;
+    Context(Context&&) = delete;
+
+public:
+    const ERL_NIF_TERM ok;
+
+public:
+    /**
+     * Get a reference to the context associated with the current environment.
+     */
+    static Context&
+    in(ErlNifEnv* env)
+    {
+        return *reinterpret_cast<Context*>(enif_priv_data(env));
+    }
+
+    /**
+     * Passed to ERL_NIF_INIT to load the context as private data.
+     */
+    static int
+    load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+    {
+        try
+        {
+            *priv_data = (void*)new Context{env};
+        }
+        catch (std::exception& ex)
+        {
+            std::cout
+                << "Failed to initialize context! "
+                << ex.what()
+                << std::endl;
+            return 1;
+        }
+        std::cout << "Context initialized" << std::endl;
+        return 0;
+    }
+
+    /**
+     * Passed to ERL_NIF_INIT to delete the context stored in private data.
+     */
+    static void
+    unload(ErlNifEnv* env, void* priv_data)
+    {
+        delete (Context*)priv_data;
+    }
+};
+
 static ERL_NIF_TERM
 open_window(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
     SDL_Window* window = SDL_CreateWindow(
-        "mywindow",
+        "NatVis",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
         480,
-        SDL_WINDOW_SHOWN);
-    SDL_Delay(2000);
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    SDL_Delay(1000);
 
     SDL_DestroyWindow(window);
 
-    SDL_Quit();
-
-    return enif_make_atom(env, "ok");
+    return Context::in(env).ok;
 }
+
 
 static ErlNifFunc exported_funcs[] = {
     // { name, airity, function }
     {"open_window", 0, open_window},
 };
 
-ERL_NIF_INIT(Elixir.NatVis, exported_funcs, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(
+    Elixir.NatVis,
+    exported_funcs,
+    Context::load,
+    NULL,
+    NULL,
+    Context::unload)
 
