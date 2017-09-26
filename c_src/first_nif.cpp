174e29aa956fdcbbd46c4c17b66cf48ecd81bc82
Bradley Lyman <lyman.brad3211@gmail.com>
2017-09-25 23:06:56 -0700

    First SDL2 Window

--- c_src/first_nif.cpp
+++ c_src/first_nif.cpp
@@ -1,8 +1,24 @@
 #include <erl_nif.h>
+#include <SDL.h>
 
 static ERL_NIF_TERM
 open_window(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "mywindow",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN);
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return enif_make_atom(env, "ok");
 }
 
